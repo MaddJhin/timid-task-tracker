01940781c88053a9e1986d079983eebb8e98ae33
Francisco Carrera <fc1159@nyu.edu>
2017-10-05 14:10:49 -0400

    Added post field form. Able to hit post endpoint. Able to hit get endpoint. Form sends information

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,7 +15,14 @@ router.use(function timeLog(req, res, next) {
 // Routing
 // ======================================
 router.get('/', function (req, res) {
+    // TO DO: wrap render in a query
     res.render('index');
 });
+
+router.post('/', function (req, res) {
+
+    // TO DO: Wrap redirect in a query
+    res.redirect('/');
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded( { extended: true } ));
-app.use(bodyParser(JSON));
+app.use(bodyParser.json());
 
 // Set Handlebars as the default templating engine
 // =======================================================
